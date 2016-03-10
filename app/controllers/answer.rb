fb3e20a8072412295bb8fdca9f8d4fe7ad73ab66
Joseph Scott <email": null>
2016-03-09 21:54:33 -0600

    add all answers and single answer views

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -1,26 +1,29 @@
 get '/answers' do
-  @question_answers = Answer.all
-  erb :'/answers'
+  @question = Question.all
+  erb :'answers/index'
+end
+
+post '/answers' do
+  redirect '/answers'
 end
 
 get '/answers/:id/vote' do
   @answer = Answer.find(params[:id])
-  @answer << Vote.new
+  @answer.votes << Vote.new
   redirect "/answers"
+end
+
+get '/answers/:id' do
+  @answer = Answer.find_by(id: params[:id])
+  if @answer
+    erb :'/answers/show'
+  else
+    erb :'/answers'
+  end
 end
 
 delete '/answers/:id' do
   @answer = Answer.find(params[:id])
   @answer.destroy
   redirect '/answers'
-end
-
-# post '/answers' do
-#
-#   redirect '/answers'
-# end
-
-get '/answer/:id' do
-  @answer = Answer.find(params[:id])
-  erb :'/answer/show'
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-author = User.create(username: "Momo", password: "password")
-responder = User.create(username: "Emily", password: "password")
-question = Question.create(title: "Hi", body: "Budyyyyy", author: author)
-answer = Answer.create(body: "My answer", author: responder, question: question)
+# author = User.create(username: Faker::Hipster.word, password: "password")
+# responder = User.create(username: Faker::Hipster.word, password: "password")
+# question = Question.create(title: Faker::Hipster.word, body: Faker::Hipster.sentences, author: author)
+# answer = Answer.create(body: Faker::Hipster.sentences, author: responder, question: question)
 
-question.comments << Comment.new(body: "This is a comment", commentor: responder)
-question.votes << Vote.new(voter: responder)
+# question.comments << Comment.new(body: Faker::Hipster.sentences, commentor: responder)
+# question.votes << Vote.new(voter: responder)
+
+# 5.times do
+#   user = User.create(username: Faker::Internet.user_name, password: "password")
+#   5.times do
+#     question = user.questions.create(title: Faker::Lorem.sentence, body: Faker::Lorem.paragraph, user_id: 2)
+#     5.times do
+#       question.answers.create(body: Faker::Lorem.sentence, user_id: 3)
+#     end
+#   end
+# end
