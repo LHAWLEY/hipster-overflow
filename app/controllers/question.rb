5ae50c7c7fbbdd401f62a1cb4830d5c0c91c2ffb
Emily <ebosakowski@gmail.com>
2016-03-10 09:19:12 -0600

    Add question index, remove unnecessary question routes and views

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -4,24 +4,9 @@ get '/questions' do
   erb :'index'
 end
 
-get '/questions/new' do
-  erb :'questions/new'
-end
-
 # only users should be able to add new questions
 # will put an if/else at the top of the new question form asking for session permission
 # Need to include user id field for login
-post '/questions/new' do
-  "hello"
-  @question = Question.new(title: params[:title], body: params[:body] , author: current_user)
-  if @question.save
-    redirect '/'
-  else
-    @errors = @question.errors.full_messages
-    "something fucked up"
-    erb :'questions/new'
-  end
-end
 
 get '/questions/:id' do
   @question = Question.find(params[:id])
