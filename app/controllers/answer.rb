b73b2909f7b98771f07dc432ca92296e6f7d91d9
LeeAnne Hawley & Prince Sadie <pair+lhawley.princesadie@devbootcamp.com>
2016-03-10 15:00:58 -0600

    Add styling to questions page

--- app/controllers/answer.rb
+++ app/controllers/answer.rb
@@ -7,17 +7,21 @@ get '/answers/new' do
   erb :'/answers/new'
 end
 
-
-
 get '/answers/:id' do
   @answer = Answer.find(params[:id])
   erb :'/answers/show'
 end
 
 get '/answers/:id/vote' do
-  # @answer = Answer.find(params[:id]) should be user
-  @answer.votes << Vote.new#(user)
-  redirect "/answers"
+  answer = Answer.find(params[:id])
+  redirect "/questions/#{answer.question.id}" if !logged_in?
+  if answer.votes.find_by(user_id: current_user.id) != nil
+    redirect "/questions/#{answer.question.id}"
+  else
+    new_vote = Vote.new(voter: current_user)
+    answer.votes << new_vote
+    redirect "/questions/#{answer.question.id}"
+  end
 end
 
 # for editing an answer
