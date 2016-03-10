8f508f03c7209ada8e6145a26390d7622316d8c6
Emily <ebosakowski@gmail.com>
2016-03-09 19:23:54 -0600

    Add new question form, show view, and question list helper

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,26 +1,28 @@
-get '/' do
-  redirect '/questions'
+
+# get '/questions' do
+#   erb :index
+# end
+
+get '/questions/new' do
+  erb :'questions/new'
 end
 
-get '/questions' do
-  erb :index
+# only users should be able to add new questions
+# will put an if/else at the top of the new question form asking for session permission
+post '/questions' do
+  @question = Question.new(title: title, body:body)
+  if @question.save
+    redirect '/'
+  else
+    "something fucked up"
+    erb :errors
+  end
 end
 
-get '/questions/:id/vote' do
-
-  redirect "/questions"
+get '/question/:id' do
+  erb :'questions/show'
 end
 
 delete '/questions/:id' do
   # write logic for deleting questions here.
-end
-
-post '/questions' do
-
-  redirect '/questions'
-end
-
-get '/question/:id' do
-
-  erb :question
 end
